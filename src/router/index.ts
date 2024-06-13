f7aea723a16ac2ebb2cc1420a7e0a7ec0b7203e9
Vladislav1992 <kuryanov.vlad@yandex.ru>
2024-06-13 20:34:52 +0300

    added api + user page

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,6 +26,14 @@ const router = createRouter({
       component: () => import('../views/SignUp.vue'),
     },
     {
+      path: '/signin',
+      name: 'signin',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/SignIn.vue'),
+    },
+    {
       path: '/cars',
       name: 'cars',
       // route level code-splitting
--- src/views/Cars.vue
+++ src/views/Cars.vue
@@ -1,5 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+import axios from 'axios';
+import '../api/api.ts';
 
-<template></template>
+const authStore = useAuthStore();
+const cars = ref();
+
+const getAllCars = async () => {
+  try {
+    // const response = await axios.get(
+    //   `https://jwt-firebase-46e88-default-rtdb.europe-west1.firebasedatabase.app/cars.json?auth=${
+    //     authStore.userInfo.token
+    //   }`,
+    // );
+    const response = await axios.get(
+      `https://jwt-firebase-46e88-default-rtdb.europe-west1.firebasedatabase.app/cars.json`,
+    );
+    cars.value = response.data;
+  } catch (err: any) {
+    console.log(err.response);
+  }
+};
+
+onMounted(async () => {
+  await getAllCars();
+});
+</script>
+
+<template>
+  <div>
+    <h2>Cars</h2>
+    <div v-for="(car, i) in cars">
+      <div>{{ car.name }}</div>
+      <div>{{ car.type }}</div>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
+
+<!-- https://console.firebase.google.com/project/jwt-firebase-46e88/database/jwt-firebase-46e88-default-rtdb/data/~2F -->
